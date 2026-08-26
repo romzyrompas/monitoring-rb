cea6bfea6098a2e2e018b22fb4d71619abd0183e
ridelrompas10-design <ridelrompas10@gmail.com>
2026-08-26 12:08:43 +0800

    fix: resolve 403 forbidden error by implementing FilamentUser

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,9 +10,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+// 1. Tambahkan dua baris ini untuk Filament
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+// 2. Tambahkan "implements FilamentUser" di baris class
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -28,5 +33,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // 3. Tambahkan fungsi ini di bagian paling bawah
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Untuk saat ini, kita kembalikan "true" agar Anda dan Coach bisa langsung masuk
+        return true; 
     }
 }
